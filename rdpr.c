c8a1e40743732ab4e1174b618205f852326d434b
Torrey Randolph <Torrey Randolph>
2017-03-20 20:54:33 -0700

    updates

--- rdpr.c
+++ rdpr.c
@@ -114,8 +114,8 @@ void setHeader(char* buffer) {
 	header.data_len    = atoi(tokens[4]);
 	header.window_size = atoi(tokens[5]);
 	
-	if (i == 6) strcpy(buffer, "");
-	else strcpy(buffer, tokens[6]);
+	//if (i == 6) strcpy(buffer, "");
+	//else strcpy(buffer, tokens[6]);
 	
 	printf("buffer = %s\n", buffer);
 }
