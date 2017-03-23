83c9c143dac21757be9a367c219a38f2553a4d69
Torrey Randolph <Torrey Randolph>
2017-03-22 17:07:58 -0700

    updates

--- rdps.c
+++ rdps.c
@@ -303,6 +303,7 @@ bool sendData(int sock) {
     for (i = 0; i < WINDOW_SIZE; i ++) {
         resp = sendResponse(sock, header.seq_num);
         if ((resp == false) && (sent_entire_file == true)) {
+            printf("sent_entire_file = %s\n", sent_entire_file ? "true" : "false");
             return false;
         } else if ( resp == false ) {
             break;
