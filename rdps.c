d140f4632c94fda74f030f3b40cccd07430d574a
Torrey Randolph <Torrey Randolph>
2017-03-24 02:10:39 -0700

    updates

--- rdps.c
+++ rdps.c
@@ -33,7 +33,7 @@ bool connection();
 bool createServer();
 
 // Global Constants
-#define MAX_DATA_SIZE   200
+#define MAX_DATA_SIZE   300
 #define MAX_BUFFER_SIZE 1024
 #define WINDOW_SIZE 300          // NOTE: 300 works fine. With 400, the last few packets come
                                 // out of order and/or not in time.
