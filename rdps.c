ed19999c3b8d6467b445985695c42dccf6e1ed0f
Torrey Randolph <Torrey Randolph>
2017-03-24 15:45:03 -0700

    updates

--- rdps.c
+++ rdps.c
@@ -33,7 +33,7 @@ bool connection();
 bool createServer();
 
 // Global Constants
-#define MAX_DATA_SIZE   500
+#define MAX_DATA_SIZE   900
 #define MAX_BUFFER_SIZE 1024
 #define WINDOW_SIZE     10
 
